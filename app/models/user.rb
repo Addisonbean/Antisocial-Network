92b697326719ca911809d71f6e5e0242242626e4
Addison Bean <addisonbean@gmail.com>
2017-02-01 16:02:01 -0600

    Add a user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,24 @@
 class User < ApplicationRecord
 	has_many :posts
 
+	before_save { self.email = email.downcase }
+
 	validates :email, presence: true, length: { maximum: 256 }, uniqueness:
-		{ case_sensitive: false }, format: { with: /.+@.+/ }
+		{ case_sensitive: false }, format: { with: /.+@.+\.[A-Za-z0-9\-]+/ }
 	validates :username, presence: true, length: { maximum: 20 }, uniqueness:
 		{ case_sensitive: false }
 
 	has_secure_password
 
+	validates :password, length: { minimum: 6 }, allow_nil: true
+
 	def to_param
 		username
 	end
+
+	def User.digest(str)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(str, cost: cost)
+	end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:valid_user)
+	end
+
+	test "valid user signup" do
+		assert @user.valid?
+	end
+
+	test "signup with invalid email" do
+		@user.email = "meh@"
+		assert_not @user.valid?
+
+		@user.email = "@meh"
+		assert_not @user.valid?
+
+		@user.email = "meh@mail"
+		assert_not @user.valid?
+
+		@user.email = "q" * 247 + "@gmail.com"
+		assert_not @user.valid?
+	end
+
+	test "signup with invalid username" do
+		@user.username = "q" * 21
+		assert_not @user.valid?
+	end
+
+	test "signup with duplicate information" do
+		user2 = @user.dup
+		@user.save
+		assert_not user2.valid?
+	end
+
 end
