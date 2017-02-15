7da3ee5d8b48bb4452e3fc2a5044600d6dff3e81
Addison Bean <addisonbean@gmail.com>
2017-02-14 18:50:35 -0600

    Make the feed a tad bit more suave

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
 	def home
 		if logged_in?
-			@feed_items = current_user.posts
+			#@feed_items = current_user.posts
+			@feed_items = Post.all
 			@post = current_user.posts.build
 		end
 	end
